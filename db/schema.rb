d3b5cdb6001744fb550546c59a0afc82f2f1aabf
Georgy <geovo851@gmail.com>
2015-09-02 08:20:11 -0700

    Add style products

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20150902142323) do
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
-    t.decimal  "pice",        precision: 8, scale: 2
+    t.decimal  "price",       precision: 8, scale: 2
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
