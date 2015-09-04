91332670a2847a1612274d210db100818102f026
Georgy <geovo851@gmail.com>
2015-09-04 02:12:14 -0700

    Add orders

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   # GET /products
@@ -61,6 +63,16 @@ class ProductsController < ApplicationController
     end
   end
 
+  def who_bought
+    @product = Product.find(params[:id])
+    @latest_order = @product.orders.order(:updated_at).last
+    if stale?(@latest_order)
+      respond_to do |format|
+        format.atom
+      end
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
@@ -72,3 +84,4 @@ class ProductsController < ApplicationController
       params.require(:product).permit(:title, :description, :image_url, :price)
     end
 end
+
